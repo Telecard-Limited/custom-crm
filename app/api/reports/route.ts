d9b4c363960a990be68593db8788610ffd53942a
Rishabh-Chowdhry <rishabh_chowdhry@telecard.com.pk>
2023-10-09 13:18:46 +0500

    remove unused models and api

--- app/api/reports/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function Handler(request: NextRequest) {
-  try {
-  } catch (e: any) {}
-}
