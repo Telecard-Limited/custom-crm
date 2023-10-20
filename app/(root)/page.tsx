7b501faa2b7f92266f35eb34bc62510691cb07e0
Rishabh-Chowdhry <rishabh_chowdhry@telecard.com.pk>
2023-10-20 16:27:20 +0500

    routelink

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import Button from "@/components/Button";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center h-[20vh] mt-6">
       <h1 className="text-4xl font-extrabold text-white ">Beautix CRM</h1>
-      <Button label="Get Started" outline onClick={() => {}} className="mt-5" />
+      <Button
+        label="Get Started"
+        outline
+        onClick={() => router?.push("/registeration")}
+        className="mt-5"
+      />
     </div>
   );
 }
