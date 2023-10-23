2f491b13eff5a0212868e1676ff1145deef80b33
Rishabh-Chowdhry <rishabh_chowdhry@telecard.com.pk>
2023-10-23 12:20:41 +0500

    changes in next config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "http://192.168.150.151:3000/",
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
