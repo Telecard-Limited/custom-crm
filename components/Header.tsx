08413475bee342daa103cebd2f36aa477e135e2c
Rishabh-Chowdhry <rishabh_chowdhry@telecard.com.pk>
2023-10-25 14:57:41 +0500

    logo removed and updated reference citation

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,16 +6,6 @@ const Header = () => {
   const router = useRouter();
   return (
     <div className="container fixed flex flex-row items-center justify-between w-full gap-4 bg-transparent cursor-pointer ">
-      <Image
-        src="/images/logo.jpg"
-        height={100}
-        width={100}
-        style={{
-          width: "50%",
-          height: "50%",
-        }}
-        alt=""
-      />
       <MenuItem label="Pricing" onClick={() => {}} />
       <MenuItem label="About" onClick={() => {}} />
       <MenuItem label="Sign In" onClick={() => router.push("/signin")} />
