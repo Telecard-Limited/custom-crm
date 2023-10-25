f1e47d3da8241f82ba6237a584f6755dab0a0649
Rishabh-Chowdhry <rishabh_chowdhry@telecard.com.pk>
2023-10-25 15:03:15 +0500

    header .tsx  navbar align ment changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Header = () => {
   const router = useRouter();
   return (
-    <div className="container fixed flex flex-row items-center justify-between w-full gap-4 bg-transparent cursor-pointer ">
+    <div className="container fixed flex flex-row items-end justify-end w-full gap-4 bg-transparent cursor-pointer ">
       <MenuItem label="Pricing" onClick={() => {}} />
       <MenuItem label="About" onClick={() => {}} />
       <MenuItem label="Sign In" onClick={() => router.push("/signin")} />
